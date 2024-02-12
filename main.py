e5a6fa6ce74a2b00bd2a59d97b979f7a83e50b51
Yeshwanth M R <127304570+Royal-Phoenix@users.noreply.github.com>
2024-02-12 19:11:52 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from model import Graph
+import tkinter as tk
+
+
+window = tk.Tk()
+window.title("Dijkstra's GUI")
+window.geometry('1366x788')
+canvas = tk.Canvas(window)
+canvas.pack(fill="both", expand=True)
+obj = Graph(9, canvas, window)
+submit = tk.Button(window, text='SUBMIT', command=lambda: obj.Submit(), bg='cyan', activebackground='green', font=('times', 25, 'bold'))
+submit.place(x=1150, y=600)
+window.mainloop()
